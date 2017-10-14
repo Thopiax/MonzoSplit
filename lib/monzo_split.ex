5f6a2403a4fc69656eb61b0ecc6522fbf6537a54
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-10-14 20:20:39 +0100

    Initial commit

---
+++ lib/monzo_split.ex
@@ -0,0 +1,18 @@
+defmodule MonzoSplit do
+  @moduledoc """
+  Documentation for MonzoSplit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MonzoSplit.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/monzo_split_test.exs
@@ -0,0 +1,8 @@
+defmodule MonzoSplitTest do
+  use ExUnit.Case
+  doctest MonzoSplit
+
+  test "greets the world" do
+    assert MonzoSplit.hello() == :world
+  end
+end
