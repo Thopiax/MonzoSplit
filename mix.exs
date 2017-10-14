91206b5c14b0a871ffd0748075e48de62a2d9d5b
Norbert Podsadowski <norbo11@googlemail.com>
2017-10-14 22:04:50 +0100

    Added oauth2 library to deps

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule MonzoSplit.Mixfile do
   def application do
     [
       mod: {MonzoSplit.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :oauth2]
     ]
   end
 
@@ -41,7 +41,8 @@ defmodule MonzoSplit.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:drab, "~> 0.5"}
+      {:drab, "~> 0.5"},
+      {:oauth2, "~> 0.9"}
     ]
   end
 
