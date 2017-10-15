ffb28fcc834764ab8b1ef6eeb2f7c7ac0802c899
Norbert Podsadowski <norbo11@googlemail.com>
2017-10-15 23:15:40 +0100

    Query Monzo API

--- lib/monzo_split_web/controllers/monzo_controller.ex
+++ lib/monzo_split_web/controllers/monzo_controller.ex
@@ -9,6 +9,21 @@ defmodule MonzoSplitWeb.MonzoController do
 
   def complete_monzo_oauth(conn, %{"code" => code}) do
     client = MonzoOAuthStrategy.get_token!(code: code)
+    %{"accounts" => accounts} = OAuth2.Client.get!(client, "/accounts").body
+
+    IO.inspect(accounts)
+
+    [account | _ ] = accounts
+
+    IO.inspect(account)
+    IO.inspect(account["id"])
+
+    response = OAuth2.Client.post!(client, "/webhooks", %{
+      account_id: account["id"],
+      url: "https://b8ccf680.ngrok.io/api/monzo/transaction"
+    })
+
+    IO.inspect(response)
 
     conn
       |> assign(:monzo_done, true)
